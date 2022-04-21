786beec0d156bbe3ae86d7be36156010addc179f
Vinicius L S Braz <vinicius_lsb@live.com>
2022-04-21 12:34:12 -0300

    README.md created

--- src/components/GridItem/styles.ts
+++ src/components/GridItem/styles.ts
@@ -6,12 +6,17 @@ type ContainerProps = {
 
 export const Container = styled.div<ContainerProps>`
     background-color: ${props => props.bgcolor ? '#E2E3E3' : '#53277E'};
-    height: 6.25rem;
+    height: 6.125rem;
     border-radius: 1.25rem;
     display: flex;
     justify-content: center;
     align-items: center;
     cursor: pointer;
+    border: 1px solid transparent;
+
+    &:hover{
+        border: 1px solid #E2E3E3;
+    }
 `;
 
 export const Icon = styled.img`
